a0502e1e25942da3d5c5bd8795327657a3e9b62e
Mike Shultz <shultzm@gmail.com>
2019-03-19 14:35:32 -0600

    fixes for newer version and removal of unnecessary junk

--- pelicanconf.py
+++ pelicanconf.py
@@ -27,17 +27,17 @@ ASSET_CONFIG = (
 THEME_STATIC_DIR = 'static'
 
 # Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
-TRANSLATION_FEED_ATOM = None
-AUTHOR_FEED_ATOM = None
-AUTHOR_FEED_RSS = None
+# FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
+# TRANSLATION_FEED_ATOM = None
+# AUTHOR_FEED_ATOM = None
+# AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+# LINKS = (('Pelican', 'http://getpelican.com/'),
+#          ('Python.org', 'http://python.org/'),
+#          ('Jinja2', 'http://jinja.pocoo.org/'),
+#          ('You can modify those links in your config file', '#'),)
 
 # Social widget
 # SOCIAL = (('You can add links in your config file', '#'),
